255033ebf8eba10b603eecf10a90c2bf3e96693d
IssFantasy <45601003+IssFantasy@users.noreply.github.com>
2018-12-04 17:21:30 +0100

    Update botdiscord.py

--- botdiscord.py
+++ botdiscord.py
@@ -28,6 +28,10 @@ async def on_message(message):
         em = discord.Embed(description='quando il truffatore ti truffa')
         em.set_image(url='https://cdn.discordapp.com/attachments/519157517100777473/519157818046021632/monka_s.png')
         await client.send_message(message.channel, embed=em)
+    if message.content == '!younesmerda':
+        em = discord.Embed(description='lol')
+        em.set_image(url='https://cdn.discordapp.com/attachments/519159557264769038/519548128932659210/nm.jpg')
+        await client.send_message(message.channel, embed=em)
     if message.content == '!dio':
         em = discord.Embed(description='dio ti sta guardando')
         em.set_image(url='https://cdn.discordapp.com/attachments/519157517100777473/519160214893887488/jeez.jpg')
@@ -39,7 +43,7 @@ async def on_message(message):
         em.set_image(url='https://cdn.discordapp.com/attachments/519157517100777473/519169473807253511/pd.png')
         await client.send_message(message.channel, embed=em)
     if message.content == '!comandi':
-        await client.send_message(message.channel,'hacker, monkas, dio, biscottodellafortuna,biscottodellafortuna2, petriniascuola, pd')
+        await client.send_message(message.channel,'younesmerda, hacker, monkas, dio, biscottodellafortuna, petriniascuola, pd')
     if message.content == '!hacker':
         em = discord.Embed(description='quando riesci a trovare la password della scuola')
         em.set_image(url='https://cdn.discordapp.com/attachments/519157517100777473/519172372792606730/hacerman.jpg')
