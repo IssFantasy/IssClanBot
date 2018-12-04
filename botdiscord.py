eadafa22d4bb544f5037c288687a3130bc9584e5
IssFantasy <45601003+IssFantasy@users.noreply.github.com>
2018-12-04 16:32:44 +0100

    Add files via upload

---
+++ botdiscord.py
@@ -0,0 +1,45 @@
+import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+import asyncio
+import time
+import random
+from discord import Game
+
+
+Client = discord.client
+client = commands.Bot(command_prefix = '!')
+Clientdiscord = discord.Client()
+
+
+@client.event
+async def on_ready():
+    await client.change_presence(game=Game(name='tagliarsi le vene'))
+    print('Ready, Freddy') 
+
+
+@client.event
+async def on_message(message):
+    if message.content == '!biscottodellafortuna':
+        await client.send_message(message.channel,'oggi respirerai')
+    if message.content == '!monkas':
+        em = discord.Embed(description='quando il truffatore ti truffa')
+        em.set_image(url='https://cdn.discordapp.com/attachments/519157517100777473/519157818046021632/monka_s.png')
+        await client.send_message(message.channel, embed=em)
+    if message.content == '!dio':
+        em = discord.Embed(description='dio ti sta guardando')
+        em.set_image(url='https://cdn.discordapp.com/attachments/519157517100777473/519160214893887488/jeez.jpg')
+        await client.send_message(message.channel, embed=em)
+    if message.content == '!petriniascuola':
+        await client.send_message(message.channel,'avrai sbagliato nome. Petrini non viene a scoula')
+    if message.content == '!pd':
+        em = discord.Embed(description='il pd ti guarda')
+        em.set_image(url='https://cdn.discordapp.com/attachments/519157517100777473/519169473807253511/pd.png')
+        await client.send_message(message.channel, embed=em)
+    if message.content == '!comandi':
+        await client.send_message(message.channel,'hacker, monkas, dio, biscottodellafortuna, petriniascuola, pd')
+    if message.content == '!hacker':
+        em = discord.Embed(description='quando riesci a trovare la password della scuola')
+        em.set_image(url='https://cdn.discordapp.com/attachments/519157517100777473/519172372792606730/hacerman.jpg')
+        await client.send_message(message.channel, embed=em)
+client.run('NTE5MTUzMzcwNDIyODM3MjY5.DubQrg.ALKdq0sXFLR89j9vh1Y3s2zZfTk')
